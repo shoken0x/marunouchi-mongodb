10ceee570eb3f974ae81d2f50e625f9c92e01c41
syokenz <syokenz@gmail.com>
2012-11-05 21:22:06 +0900

    update

--- 20121106/syokenz/gmail_conf.rb
+++ 20121106/syokenz/gmail_conf.rb
@@ -1,7 +1,7 @@
 class GConf
   LIBPATH='D:\src\git\ruby-gmail\lib' #ruby-gmail/libへのパス
-  USERNAME='mongonouchi@gmail.com'    #Gmailのアドレス
-  PASSWORD='openstandia2012'          #Gmailのパスワード
+  USERNAME='_@gmail.com'    #Gmailのアドレス
+  PASSWORD='xxxx'          #Gmailのパスワード
   IMAGES_DIR='D:\\tmp\\'              #画像を保存するディレクトリ
 end
 
