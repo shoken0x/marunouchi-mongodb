1708d98d334c540a15faba48856acedc8452aa90
shoken fujisaki <syokenz@gmail.com>
2012-11-05 21:24:47 +0900

    Update 20121106/syokenz/gmail_conf.rb

--- 20121106/syokenz/gmail_conf.rb
+++ 20121106/syokenz/gmail_conf.rb
@@ -1,7 +1,7 @@
 class GConf
   LIBPATH='D:\src\git\ruby-gmail\lib' #ruby-gmail/libへのパス
-  USERNAME='_@gmail.com'    #Gmailのアドレス
-  PASSWORD='xxxx'          #Gmailのパスワード
+  USERNAME='_@gmail.com'              #Gmailのアドレス
+  PASSWORD='xxxx'                     #Gmailのパスワード
   IMAGES_DIR='D:\\tmp\\'              #画像を保存するディレクトリ
 end
 
